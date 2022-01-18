c58dfb1469bfd0fb009ebe290e7c36704f335e55
ana-maria-b <82262717+ana-maria-b@users.noreply.github.com>
2022-01-18 12:28:15 +0000

    created components and some functionality

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,22 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Button from "../Button";
+import Energisers from "../Energisers";
+import Quotes from "../Quotes";
+import Reminders from "../Reminders";
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [id, setId] = useState(1);
 
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
-  }, []);
+  }, [id]);
 
+  function zenButton(event) {
+    const randomId = Math.floor(Math.random() * 10);
+    setId(randomId);
+    //write all 3 functions in here
+    // console.log(randomId);
+  }
   return (
-    <div className="Appy times">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div>
+      <Button onClick={zenButton} text="Zen-Time"></Button>
+      <Energisers text="Energise!"></Energisers>
+      <Quotes text="Your daily good vibes"></Quotes>
+      <Reminders text="Remember!"></Reminders>
     </div>
   );
 }
--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,1 +1,10 @@
-import './styles.css';
+import "./styles.css";
+import React from "react";
+
+export default function Button({ zenButton, text }) {
+  return (
+    <div>
+      <button onClick={zenButton}>{text}</button>
+    </div>
+  );
+}
