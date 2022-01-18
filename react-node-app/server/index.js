8f534de552fd596d0be5558e1b8fe807c1223b0e
ana-maria-b <82262717+ana-maria-b@users.noreply.github.com>
2022-01-18 14:47:35 +0000

    server side connection check

--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -1,5 +1,4 @@
-
-import express from"express";
+import express from "express";
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,8 +8,10 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get("/energisers/1", (req, res) => {
+  res.json({ message: "Appy days!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-
