c8a677e901c06c7bb5dacecfde605c4f3d1e901b
RobertHJones <roberthuwjones@outlook.com>
2022-01-19 11:39:49 +0000

    energiser+quote generating

--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 
-
 import indexRouter from "../routes/index.js";
-
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+// app.use(function (req, res, next) {
+//   req.set("Access-Control-Allow-Origin", "*");
+//   next();
+// });
 
 // app.use(logger("dev"));
 app.use(express.json());
@@ -16,14 +18,17 @@ app.use(express.static("public"));
 
 app.use("/", indexRouter);
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+// app.get("/api", (req, res) => {
+//   res.json({ message: "Hello from server!" });
+// });
+
+app.get("/energisers/3", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.json({ message: "Appy days!" });
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-
 export default app;
-
