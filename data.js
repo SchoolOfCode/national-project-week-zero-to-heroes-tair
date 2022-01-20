776617c665f25da6cf7075d16b52454864fcfb47
RobertHJones <roberthuwjones@outlook.com>
2022-01-20 21:18:17 +0000

    added to data

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const energisers = [
   {
     id: 1,
-    activity: "Play gartic",
+    activity: "Play a game of gartic",
     category: "game",
     url: "https://gartic.io/",
     // favourite: false
@@ -34,10 +34,46 @@ export const energisers = [
     url: "http://iteslj.org/games/9894.html",
     // favourite: false
   },
+  {
+    id: 6,
+    activity: "Follow this meditation session",
+    category: "wellbeing",
+    url: "https://www.youtube.com/watch?v=y2RAEnWreoE",
+    // favourite: false
+  },
+  {
+    id: 7,
+    activity:
+      "Go for a walk and take a photo of the most interesting thing you see",
+    category: "physical",
+    url: "https://www.getthegloss.com/article/genius-ways-to-make-your-daily-walk-more-interesting-that-we-bet-you-hadn-t-thought-of",
+    // favourite: false
+  },
+  {
+    id: 8,
+    activity: "Play a game of Scattergories",
+    category: "game",
+    url: "https://scattergoriesonline.net/",
+    // favourite: false
+  },
+  {
+    id: 9,
+    activity: "Arrange a virtual scavenger hunt",
+    category: "game",
+    url: "https://www.sessionlab.com/methods/virtual-scavenger-hunt",
+    // favourite: false
+  },
+  {
+    id: 10,
+    activity: "Play a game of Scattergories",
+    category: "game",
+    url: "https://scattergoriesonline.net/",
+    // favourite: false
+  },
 ];
 
 export const reminders = [
-  { id: 1, reminder: "Drink water" },
+  { id: 1, reminder: "Drink some water" },
   { id: 2, reminder: "Take a break from the screen" },
   { id: 3, reminder: "Stretch your legs" },
   { id: 4, reminder: "Look out the window and enjoy the view" },
@@ -80,5 +116,18 @@ export const quotes = [
   {
     id: 7,
     quotation: "We cannot become what we want by remaining what we are",
+  },
+  {
+    id: 8,
+    quotation: "Work won't kill but worry will",
+  },
+  {
+    id: 9,
+    quotation: "The world is theirs who enjoys it",
+  },
+  {
+    id: 10,
+    quotation:
+      "You must understand that there is more than one path to the top of the mountain",
   },
 ];
