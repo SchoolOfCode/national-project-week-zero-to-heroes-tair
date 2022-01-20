23a0bc4809ec49029095c21fdfcb55ace19af484
RobertHJones <roberthuwjones@outlook.com>
2022-01-20 10:04:30 +0000

    separate repo

--- client/src/components/Title/index.js
+++
@@ -1,7 +0,0 @@
-import "./styles.css";
-import React from "react";
-import titleImage from "./title.png"
-
-export default function Title() {
-  return <div id="title1"><img src={titleImage} alt="title"/></div>;
-}
