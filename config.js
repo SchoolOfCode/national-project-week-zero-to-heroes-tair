ca1f844483dd49b7f6a17a4be1cee3bf98f53d70
RobertHJones <roberthuwjones@outlook.com>
2022-01-20 11:54:17 +0000

    databaseURL

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ export const dbname = process.env.PGDATABASE;
 export const host = process.env.PGHOST;
 export const password = process.env.PGPASSWORD;
 export const port = process.env.PGPORT;
+export const databaseURL = process.env.DATABASE_URL;
 
 console.log(dbname);
--- db/index.js
+++ db/index.js
@@ -1,14 +1,22 @@
 // Create Pool to connect to Heroku
 import pg from "pg";
-import { host, dbname, username, password, port } from "../config.js";
+import {
+  host,
+  dbname,
+  username,
+  password,
+  port,
+  databaseURL,
+} from "../config.js";
 
 const pool = new pg.Pool({
   // data from heroku
-  user: username,
-  host: host,
-  database: dbname,
-  password: password,
-  port: port,
+  // user: username,
+  // host: host,
+  // database: dbname,
+  // password: password,
+  // port: port,
+  connectionString: databaseURL,
   ssl: { rejectUnauthorized: false },
 });
 
