99451eb45eac396d6b790bb1f8afbe86b2621ad2
RobertHJones <roberthuwjones@outlook.com>
2022-01-20 11:43:50 +0000

    devscript

--- data.js
+++ data.js
@@ -1,34 +1,59 @@
 export const energisers = [
   {
     id: 1,
-    activity: "play gartic",
+    activity: "Play gartic",
     category: "game",
-    url: "www.gartic.com",
+    url: "https://gartic.io/",
     // favourite: false
   },
   {
     id: 2,
-    activity: "short yoga session",
+    activity: "Do a short yoga session",
     category: "physical",
     url: "https://www.youtube.com/watch?v=lJi17P7iKNM",
     // favourite: false
   },
   {
     id: 3,
-    activity: "draw what you did today",
+    activity: "Draw what you did today",
     category: "artistic",
     url: "https://www.youtube.com/watch?v=sjkrrmBnpGE",
     // favourite: false
   },
+  {
+    id: 4,
+    activity: "Play fruit ninja",
+    category: "game",
+    url: "https://www.agame.com/game/fruit-ninja",
+    // favourite: false
+  },
+  {
+    id: 5,
+    activity: "Have a game of fold-over stories",
+    category: "game",
+    url: "http://iteslj.org/games/9894.html",
+    // favourite: false
+  },
 ];
-
 
 export const reminders = [
-  { id: 1, reminder: "drink water" },
-  { id: 2, reminder: "take a break from the screen" },
-  { id: 3, reminder: "stretch your legs" },
+  { id: 1, reminder: "Drink water" },
+  { id: 2, reminder: "Take a break from the screen" },
+  { id: 3, reminder: "Stretch your legs" },
+  { id: 4, reminder: "Look out the window and enjoy the view" },
+  { id: 5, reminder: "Grab a healthy snack" },
+  { id: 6, reminder: "Catch up with a friend" },
+  { id: 7, reminder: "Take five deep breaths" },
+  { id: 8, reminder: "Be aware of your posture" },
+  {
+    id: 9,
+    reminder: "Congratulate yourself on something that went well today",
+  },
+  {
+    id: 10,
+    reminder: "Cuddle your pet. If you don't have a pet, grab your teddy",
+  },
 ];
-
 
 export const quotes = [
   { id: 1, quotation: "Only a mediocre person is always at their best" },
@@ -37,5 +62,23 @@ export const quotes = [
     id: 3,
     quotation:
       "Nothing in this world is harder than speaking the truth, nothing is easier than flattery",
+  },
+  {
+    id: 4,
+    quotation:
+      "Wisdom is not a product of schooling but of the lifelong attempt to acquire it",
+  },
+  {
+    id: 5,
+    quotation:
+      "Change is hard at first, messy in the middle and gorgeous in the end",
+  },
+  {
+    id: 6,
+    quotation: "New beginnings are often disguised as painful endings",
+  },
+  {
+    id: 7,
+    quotation: "We cannot become what we want by remaining what we are",
   },
 ];
