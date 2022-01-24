8d737defa7fb7224ca56d6d0d08a99b3b8d291a2
ingasafranova <sfrnv@yahoo.co.uk>
2022-01-24 18:30:52 +0000

    changes

--- server/index.js
+++ server/index.js
@@ -6,19 +6,20 @@ import indexRouter from "../routes/index.js";
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
 app.use(function (req, res, next) {
-  req.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use(
-  cors({
-    origin: [
-      "https://zen-ergise.netlify.app/",
-      "https://zen-ergise.herokuapp.com/",
-    ],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://zen-ergise.netlify.app/",
+//       "https://zen-ergise.herokuapp.com/",
+//     ],
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,10 +28,10 @@ app.use(express.static("public"));
 
 app.use("/", indexRouter);
 
-app.get("/energisers/3", (req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.json({ message: "Appy days!" });
-});
+// app.get("/energisers/3", (req, res, next) => {
+//   res.set("Access-Control-Allow-Origin", "*");
+//   res.json({ message: "Appy days!" });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
