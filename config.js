8cbc2a6957eaecc3818fae2c476d504075c5c3db
ingasafranova <sfrnv@yahoo.co.uk>
2022-04-04 12:54:01 +0100

    config

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
+let config = dotenv.config();
+
 export const username = process.env.PGUSER;
+console.log(username);
 export const dbname = process.env.PGDATABASE;
 export const host = process.env.PGHOST;
 export const password = process.env.PGPASSWORD;
